5338676135d225ad41cb02ed87a3afe15636a282
Young Lee <syounglee012@gmail.com>
2023-04-19 22:11:58 -0700

    update

--- src/pages/about-us/index.js
+++ src/pages/about-us/index.js
@@ -12,7 +12,7 @@ export default function AboutUs() {
         src={bannerImage}
         alt={"about-us landing page banner image"}
         title="ABOUT US"
-        left="70px"
+        left="150px"
       />
       <Container>
         <div className="center-wrapper">
--- src/pages/contact-us/index.js
+++ src/pages/contact-us/index.js
@@ -11,7 +11,7 @@ export default function ContactUs() {
         src={bannerImage}
         alt={"attorney landing page banner image"}
         title="CONTACT US"
-        left="60px"
+        left="180px"
       />
       <Container>
         <div className="center-wrapper">
