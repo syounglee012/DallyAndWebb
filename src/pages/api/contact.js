492ebaa4916103cdbb8a52c65e542255b2da6368
Young Lee <syounglee012@gmail.com>
2023-05-10 11:11:09 -0700

    update var

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -47,7 +47,7 @@ export default async function contact(req, res) {
     try {
       await transporter.sendMail({
         from: email,
-        to: [email, email1],
+        to: email,
         subject: `Message From ${data.name}`,
         text: `Sent from: ${data.name},${data.email}`,
         html: `<p>Sent from: ${data.name},${data.email}</p>
